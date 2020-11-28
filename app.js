5f2b00dc00d10048f0f5baa2e2b2bd1224603130
candrianne <49613003+candrianne@users.noreply.github.com>
2020-11-28 22:02:29 +0100

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const Router = require('./route');
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+require('dotenv').config();
+
+
+app.use(express.json());
+app.use(Router);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
