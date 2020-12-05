75bde70c90e29010dcbe09d4e9c5cf3e47f42826
candrianne <49613003+candrianne@users.noreply.github.com>
2020-12-05 03:24:42 +0100

    Add files via upload
    userChallengeScore

--- modele/userChallengeDB.js
+++ modele/userChallengeDB.js
@@ -1,8 +1,9 @@
 module.exports.getChallenges = async(id, client) => {
     return await client.query(`
-        SELECT UserChallenge.startdate, UserChallenge.enddate, Challenge.name
+        SELECT UserChallenge.startdate, UserChallenge.enddate, Challenge.name, DifficultyLevel.score
         FROM UserChallenge
         INNER JOIN Challenge ON UserChallenge.challengeid = Challenge.id
+        INNER JOIN DifficultyLevel ON DifficultyLevel.id = Challenge.id
         WHERE UserChallenge.userid = $1`, [id]
     );
 };
