5d395d3346c6d4c1d427a72d123ef67b733db624
candrianne <49613003+candrianne@users.noreply.github.com>
2020-12-11 01:35:12 +0100

    Add files via upload

--- modele/challengeDB.js
+++ modele/challengeDB.js
@@ -1,6 +1,6 @@
 module.exports.getAllChallenges = async(client) => {
     return await client.query(`
-        SELECT Challenge.id, Challenge.name, Challenge.description, DifficultyLevel.name, Challenge.photo
+        SELECT Challenge.id, Challenge.name, Challenge.description, Challenge.photo, DifficultyLevel.name as difficulty
         FROM Challenge
         INNER JOIN DifficultyLevel ON Challenge.difficultylevelid = DifficultyLevel.id;`
     );
