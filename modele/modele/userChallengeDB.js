373f0d6fff30d639048168e104153492c888913d
candrianne <49613003+candrianne@users.noreply.github.com>
2020-12-15 18:34:18 +0100

    Delete userChallengeDB.js

--- modele/modele/userChallengeDB.js
+++
@@ -1,9 +0,0 @@
-module.exports.getAllChallenges = async(id, client) => {
-    return await client.query(`
-        SELECT UserChallenge.startdate, UserChallenge.enddate, Challenge.name, DifficultyLevel.score
-        FROM UserChallenge
-        INNER JOIN Challenge ON UserChallenge.challengeid = Challenge.id
-        INNER JOIN DifficultyLevel ON DifficultyLevel.id = Challenge.difficultylevelid
-        WHERE UserChallenge.userid = $1`, [id]
-    );
-};
