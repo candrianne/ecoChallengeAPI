cda40a406a1e5e71f3cc4aa54015e437754a56d5
candrianne <49613003+candrianne@users.noreply.github.com>
2020-12-28 23:05:49 +0100

    Add files via upload

--- modele/friendRequestDB.js
+++ modele/friendRequestDB.js
@@ -1,6 +1,6 @@
 module.exports.getAllUserFriendRequests = async(id, client) => {
     return await client.query(`
-        SELECT FriendRequest.sender, "User".photo, "User".firstname, "User".lastname
+        SELECT FriendRequest.sender, FriendRequest.receiver, "User".photo, "User".firstname, "User".lastname
         FROM FriendRequest INNER JOIN "User"
         ON FriendRequest.sender = "User".id
         WHERE FriendRequest.receiver = $1`, [id]
